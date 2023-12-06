3aab1fa7988a27bbfb0cff0d1e6ecb57e37c920f
Mariano Rubio <rubio.mariano@yahoo.com.ar>
2023-12-06 11:42:12 +0100

    modified models, changed params and configs, structured data folder including CM plots, trained model pkl files, and CSV files for training and demo

--- pseudoproof/ml_logic/model.py
+++ pseudoproof/ml_logic/model.py
@@ -45,7 +45,7 @@ def initialize_NNmodel(input_shape: tuple):
     return rnn_model
 
 
-def compile_NNmodel(model: Model, learning_rate=0.0005) -> Model:
+def compile_NNmodel(model: Model, learning_rate=0.005) -> Model:
     """
     Compile the Neural Network
     """
@@ -65,7 +65,7 @@ def fit_NNmodel(rnn_model, X_train, y_train, X_test, y_test):
     """
 
     fitted_rnn_model = rnn_model.fit(
-        X_train, y_train, epochs=800, batch_size=32, validation_data=(X_test, y_test)
+        X_train, y_train, epochs=50, batch_size=32, validation_data=(X_test, y_test)
     )
 
     print("✅ RNN model fitted")
@@ -134,14 +134,29 @@ def evaluate_RNNmodel(
 # All 5 models that we use for basic prediction baselines
 
 # define the differents models we can choose from
-knn_model = KNeighborsClassifier(n_neighbors=5)
+knn_model = KNeighborsClassifier(n_neighbors=2, p=1, weights='distance')
+#Best Parameters KNN: {'n_neighbors': 2, 'p': 1, 'weights': 'distance'}
+#Accuracy with Best Parameters KNN: 99.98%
 nb_model = GaussianNB()
-gbc_model = GradientBoostingClassifier(n_estimators=100, learning_rate=0.1)
-svm_model = SVC(kernel="rbf", C=1, random_state=42)
-rf_model = RandomForestClassifier(n_estimators=100)
+#For Naive Bayes it doesn't really make sense to make a grid search, as it only has one paramater ('priors')
+#and is usually used in specific scenarios like data imbalance (to give more weight to the minority class), or
+#weighting priors with external information that is not captured by training data (niche scenario). In any case,
+# priors are calculated automatically during training so unless there's a good reason, we shouldn't use the param.
+gbc_model = GradientBoostingClassifier(n_estimators=100, learning_rate=0.05)
+#Best Parameters for GBC: {'learning_rate': 0.05, 'n_estimators': 100}
+#Accuracy with Best Parameters for GBC: 100%
+svm_model = SVC(kernel="poly", C=10, gamma='scale')
+#Best Parameters for SVM: {'C': 10, 'gamma': 'scale', 'kernel': 'poly'}
+#Accuracy with Best Parameters for SVM: 85.88%
+rf_model = RandomForestClassifier(max_depth=None, min_samples_leaf=1, min_samples_split=2, n_estimators=50)
+#Best Parameters for Random Forest: {'max_depth': None, 'min_samples_leaf': 1, 'min_samples_split': 2, 'n_estimators': 50}
+#Accuracy with Best Parameters: 100%
+#All these results came to be from the full comprehensive dataset on a train_test split.
+#The models online won't be trained like this as it will
+
 
 # create a dict to access models both as strings and variables
-MODELS = {
+MODELS = {x§
     knn_model: "knn",
     nb_model: "naive_bayes",
     gbc_model: "gradient_boosting",
